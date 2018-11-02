4d8e4015a7deed854226a731d59f3f0b17581c65
FelipeAArruda <felipe.arruda.oktagon@gmail.com>
2018-11-02 19:39:19 -0300

    [Commit]   - Forgot last commit

---
+++ Backend/Assets/Scripts/UI/IScreenUI.cs
@@ -0,0 +1,3 @@
+﻿public interface IScreen {
+    void INIT();
+}
---
+++ Backend/Assets/Scripts/UI/MenuUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GameSparks.Api.Responses;
+
+public class MenuUI : MonoBehaviour, IScreen {
+    [SerializeField]
+    private Text mDisplayPlayerInfo;
+    [SerializeField]
+    private Button mStartButton;
+    [SerializeField]
+    private Button mLogoutButton;
+
+    public void INIT()
+    {
+        mLogoutButton.onClick = new Button.ButtonClickedEvent();
+
+        mLogoutButton.onClick.AddListener(Logout);
+    }
+
+    public void Logout()
+    {
+        GameSparksUtility.ActionLogout = delegate (EndSessionResponse pLogoutResponse) {
+            if (!pLogoutResponse.HasErrors)
+                ManagerUI.Instance.OnSucessLogout(pLogoutResponse);
+            else
+                ManagerUI.Instance.OnErrorLogout(pLogoutResponse);
+        };
+
+        GameSparksUtility.LogoutUser(GameSparksUtility.ActionLogout);
+    }
+
+    public void StartGamePlay()
+    {
+        //to do
+    }
+}
